00aa53b3669b2756ec59a03176a1e51b3f838bdc
Ignacio Ramos <ignacio.ramos@amadeus.com>
2020-02-06 18:30:12 +0100

    new version

--- src/main/java/com/herokuPOC/servlet/NewServlet.java
+++ src/main/java/com/herokuPOC/servlet/NewServlet.java
@@ -28,7 +28,7 @@ public class NewServlet extends HttpServlet {
   /**
    * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
    *
-   * @param request servlet requesttt
+   * @param request servlet request
    * @param response servlet response
    * @throws ServletException if a servlet-specific error occurs
    * @throws IOException if an I/O error occursd
