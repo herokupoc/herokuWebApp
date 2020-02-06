c34947b64382298eb365fcc839df74b2d2a180a3
Ignacio Ramos <ignacio.ramos@amadeus.com>
2020-02-06 18:44:25 +0100

    new version

--- src/main/java/com/herokuPOC/servlet/NewServlet.java
+++ src/main/java/com/herokuPOC/servlet/NewServlet.java
@@ -30,7 +30,7 @@ public class NewServlet extends HttpServlet {
    *
    * @param request servlet requestw
    * @param response servlet response
-   * @throws ServletException if a servlet-specific error occurs
+   * @throws ServletException if a servlet-specific error occursD
    * @throws IOException if an I/O error occursd
    */
   @EJB
