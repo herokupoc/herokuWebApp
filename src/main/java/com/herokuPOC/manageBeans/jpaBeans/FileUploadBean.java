10e0cbccfebfdcc207a9c1c9000bc842f3f4ccce
Jose Ferreira <inacio.ferreira@cgi.com>
2020-02-11 16:57:57 +0000

    First version of the upload

--- src/main/java/com/herokuPOC/manageBeans/jpaBeans/FileUploadBean.java
+++ src/main/java/com/herokuPOC/manageBeans/jpaBeans/FileUploadBean.java
@@ -4,6 +4,10 @@
  * and open the template in the editor.
  */
 package com.herokuPOC.manageBeans.jpaBeans;
+import com.amazonaws.auth.EnvironmentVariableCredentialsProvider;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ejb.EJB;
@@ -15,11 +19,19 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import com.herokuPOC.entity.FileContainer;
 import com.herokuPOC.services.FileUploadFacade;
+import java.io.IOException;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.List;
-
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.SdkClientException;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import java.io.File;
 /**
  *
  * @author evangelistap
@@ -28,7 +40,22 @@ import java.util.List;
 @SessionScoped
 public class FileUploadBean {
     //class variables
-    UploadedFile file;
+    private UploadedFile file;
+    private String bucketName = null;
+    // https://docs.aws.amazon.com/zh_tw/sdk-for-java/v1/developer-guide/java-dg-region-selection.html
+    private Regions clientRegion = null;
+    private String fileObjKeyName = null;
+    private AmazonS3 s3Client = null;
+    
+    public FileUploadBean(){
+        bucketName = System.getenv("S3_BUCKET_NAME");
+        System.out.println("bucketName: " + bucketName);
+        clientRegion = Regions.EU_WEST_1;
+        s3Client = AmazonS3ClientBuilder.standard()
+                .withRegion(clientRegion)
+		.withCredentials(new EnvironmentVariableCredentialsProvider())
+                .build();
+    }
     
     @EJB
     private FileUploadFacade fileuploadFacade;
@@ -87,7 +114,9 @@ public class FileUploadBean {
             System.out.println("Uploaded File Size: "+file.getSize());
             
             //if passes all validations insert on DB    
-            insertFiletoDb(file);
+            //insertFiletoDb(file);
+            
+            upload(file.getFileName());
             
             //move file to Amazon storage
             // Add message
@@ -104,4 +133,30 @@ public class FileUploadBean {
                        
 	
     }
+
+    public boolean upload(String fileName) throws IOException {
+       try {
+        // Upload a file as a new object with ContentType and title specified.
+            
+            System.out.println(this.fileName);
+            
+            PutObjectRequest request = new PutObjectRequest(bucketName, fileObjKeyName, new File(fileName));
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentType("plain/text");
+            metadata.addUserMetadata("x-amz-meta-title", "someTitle");
+            request.setMetadata(metadata);
+            s3Client.putObject(request);
+        } catch (AmazonServiceException e) {
+            // The call was transmitted successfully, but Amazon S3 couldn't process 
+            // it, so it returned an error response.
+            e.printStackTrace();
+        } catch (SdkClientException e) {
+            // Amazon S3 couldn't be contacted for a response, or the client
+            // couldn't parse the response from Amazon S3.
+            e.printStackTrace();
+        }
+       
+       return true;
+   }
+
 }
