330abe7028cd2482db18fb74f18a2dd74ebd9f5e
Jose Ferreira <inacio.ferreira@cgi.com>
2020-02-12 23:45:28 +0000

    New Class for AWS S3 upload. This class should have in the future the getFile method as well. It is not a Stateless Facade. Need to improve this.

--- src/main/java/com/herokuPOC/manageBeans/jpaBeans/FileUploadBean.java
+++ src/main/java/com/herokuPOC/manageBeans/jpaBeans/FileUploadBean.java
@@ -4,14 +4,6 @@
  * and open the template in the editor.
  */
 package com.herokuPOC.manageBeans.jpaBeans;
-import com.amazonaws.AmazonServiceException;
-import com.amazonaws.SdkClientException;
-import com.amazonaws.auth.EnvironmentVariableCredentialsProvider;
-import com.amazonaws.regions.Regions;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.PutObjectRequest;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ejb.EJB;
@@ -22,18 +14,11 @@ import org.primefaces.model.UploadedFile;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import com.herokuPOC.entity.FileContainer;
+import com.herokuPOC.services.AWSStorageFacadeTemp;
 import com.herokuPOC.services.FileUploadFacade;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.List;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.Date;
 /**
  *
  * @author evangelistap
@@ -43,16 +28,13 @@ import java.util.Date;
 public class FileUploadBean {
     //class variables
     private UploadedFile file;
-    private String fileObjKeyName = null;
-    private String bucketName = null;
-    private Regions clientRegion = null;
-    private AmazonS3 s3Client = null;
     
     @EJB
     private FileUploadFacade fileuploadFacade;
     private FileContainer fileContainer;
     private String fileName;
-    private Timestamp timeStamp = Timestamp.valueOf(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Calendar.getInstance().getTime()));
+    private final Timestamp timeStamp = Timestamp.valueOf(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Calendar.getInstance().getTime()));
+    private AWSStorageFacadeTemp aWSStorageFacade;
     
     
     public UploadedFile getFile() {
@@ -102,8 +84,9 @@ public class FileUploadBean {
             System.out.println("Uploaded File Name: "+file.getFileName());
             System.out.println("Uploaded File Size: "+file.getSize());
             
-            //if passes all validations insert on DB                
-            upload( e);
+            //if passes all validations insert on DB  
+            aWSStorageFacade = new AWSStorageFacadeTemp();
+            aWSStorageFacade.upload( e);
             // Add message
             FacesContext.getCurrentInstance().addMessage(null,new FacesMessage("File Uploaded Successfully"));
 	    FacesContext.getCurrentInstance().addMessage(null,new FacesMessage("File Uploaded Successfully"));
@@ -120,58 +103,5 @@ public class FileUploadBean {
     } 
     
     
- public boolean upload(FileUploadEvent fUEvent) throws IOException {
-       try {
-        //
-        String pattern = "yyyyMMdd"; 
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern); 
-        String date = simpleDateFormat. format(new Date()); 
-        System. out. println(date);
-           
-        String fileNameIn = fUEvent.getFile().getFileName();
-        InputStream in = fUEvent.getFile().getInputstream();
-        File tempFile = File.createTempFile("temp", "txt");
-        tempFile.deleteOnExit();
-        FileOutputStream out = new FileOutputStream(tempFile);
-        copyStream (in, out);
-        out.close();        
-        
-        // Upload a file as a new object with ContentType and title specified.
-        bucketName = System.getenv("S3_BUCKET_NAME");
-        System.out.println("bucketName: " + bucketName);
-        clientRegion = Regions.EU_WEST_1;
-        s3Client = AmazonS3ClientBuilder.standard()
-                .withRegion(clientRegion)
-		.withCredentials(new EnvironmentVariableCredentialsProvider())
-                .build();
-            System.out.println(fileNameIn);
-            //fileObjKeyName = fileNameIn.replace("\\","_");
-            //fileObjKeyName = fileNameIn.replace('/', '_');
-            //fileObjKeyName = fileNameIn.replace(' ', '_');
-            PutObjectRequest request = new PutObjectRequest(bucketName, date+"/"+fileNameIn, tempFile);
-            ObjectMetadata metadata = new ObjectMetadata();
-            metadata.setContentType("plain/text");
-            metadata.addUserMetadata("x-amz-meta-title", "someTitle");
-            request.setMetadata(metadata);
-            s3Client.putObject(request);
-            
-        } catch (AmazonServiceException e) {
-            // The call was transmitted successfully, but Amazon S3 couldn't process 
-            // it, so it returned an error response.
-            e.printStackTrace();
-        } catch (SdkClientException e) {
-            // Amazon S3 couldn't be contacted for a response, or the client
-            // couldn't parse the response from Amazon S3.
-            e.printStackTrace();
-        }
-       
-       return true;
-   }
-   public static void copyStream(InputStream in, OutputStream out) throws IOException {
-    byte[] buffer = new byte[1024];
-    int read;
-    while ((read = in.read(buffer)) != -1) {
-        out.write(buffer, 0, read);
-    }
-}
+ 
 }
